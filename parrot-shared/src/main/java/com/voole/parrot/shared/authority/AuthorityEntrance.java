2225e54e217191bc723a82517118585c932178d7
xuehuihe.java <xuehuihe.java@voole-e5cf14dc1>
2013-12-16 22:51:35 +0800

    kjh

--- parrot-shared/src/main/java/com/voole/parrot/shared/authority/AuthorityEntrance.java
+++ parrot-shared/src/main/java/com/voole/parrot/shared/authority/AuthorityEntrance.java
@@ -6,6 +6,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
@@ -31,6 +32,7 @@ public class AuthorityEntrance extends EntityHasAutoId implements
 	}
 
 	@OneToMany(mappedBy = "entrance", cascade = { CascadeType.ALL })
+	@OrderBy("pos")
 	public List<Authority> getAuthorities() {
 		return authorities;
 	}
